82c722a44637bcf0e4e4952b11f2855c083d279c
Zac Workman <zac.workman45@gmail.com>
2017-04-20 20:36:31 +0100

    Added test files for Todo components

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,29 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+const TodoList = require('TodoList');
+const Todo = require('Todo');
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
+
+  it('should render the correct number of Todo components', () => {
+    const todos = [{
+        id: 1,
+        text: 'Feed cat'
+      }, {
+        id: 2,
+        text: 'Check mail'
+      }]
+
+    let todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    let todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  });
+});
