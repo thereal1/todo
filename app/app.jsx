4b61eed3e78567a66e4e03c83861a4e5eef81d62
Zac Workman <zac.workman45@gmail.com>
2017-04-25 21:51:31 +0100

    Added store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ const {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 const TodoApp = require('TodoApp');
 
+const actions = require('actions');
+const store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Feed hamster'));
+store.dispatch(actions.setSearchText('Kill cats'));
+store.dispatch(actions.toggleShowCompleted());
+
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
