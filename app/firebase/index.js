65fe61734a1f90078bc8f7d9fcaa1b7605793862
Zac Workman <zac.workman45@gmail.com>
2017-04-28 10:38:11 +0100

    Created seperate dbs and added env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,10 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyAb8dv2zAXkd9a9ej_rREXcTrT02uSoGKk",
-    authDomain: "todo-5c57f.firebaseapp.com",
-    databaseURL: "https://todo-5c57f.firebaseio.com",
-    projectId: "todo-5c57f",
-    storageBucket: "todo-5c57f.appspot.com",
-    messagingSenderId: "951511493909"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET
   };
   firebase.initializeApp(config);
 } catch (e) {
